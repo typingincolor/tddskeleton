b9187a4aeb24e027a88d66e3e53e22d843e4c708
Andrew Braithwaite <andrewbraithwaite@rideways.com>
2019-06-20 14:28:13 +0100

    test case 4: make it pass test

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,6 +2,8 @@ public class FizzBuzz {
     public static String fizzBuzz(int i) {
         if (i==3) return "fizz";
 
+        if (i==5) return "buzz";
+
         return Integer.toString(i);
     }
 }
