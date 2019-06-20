3bd1e0f2ce83a1c0d21716eaea65eab7d80fefbc
Andrew Braithwaite <andrewbraithwaite@rideways.com>
2019-06-20 14:46:26 +0100

    test case 7: make it pass

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,6 +1,11 @@
 public class FizzBuzz {
     public static String fizzBuzz(int i) {
-        if (isMultiple(i, 3)) return "fizz";
+        if (isMultiple(i, 3)) {
+            if (isMultiple(i, 5)) {
+                return "fizzbuzz";
+            }
+            return "fizz";
+        }
 
         if (isMultiple(i,5)) return "buzz";
 
