791a5514c010762f868d6dd536a4e6cf01bc2be4
Andrew Braithwaite <andrewbraithwaite@rideways.com>
2019-06-20 14:48:35 +0100

    finish it off!

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,4 +1,10 @@
+import static java.lang.System.*;
+
 public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i=0; i <=100; i++) out.println(fizzBuzz(i));
+    }
+
     public static String fizzBuzz(int i) {
         if (isMultiple(i, 3)) {
             if (isMultiple(i, 5)) {
