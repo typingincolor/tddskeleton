bcbf2b5cd59d1dcacc2710fb06b50fb763b439fb
Andrew Braithwaite <andrewbraithwaite@rideways.com>
2019-06-20 13:58:39 +0100

    second test case: get 2 when you pass 2 passes refactor
    test helper method

--- src/test/java/TestFizzBuzz.java
+++ src/test/java/TestFizzBuzz.java
@@ -5,13 +5,18 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 public class TestFizzBuzz {
+
+    private void checkFizzBuzz(int value, String expected) {
+        assertThat(FizzBuzz.fizzBuzz(value), is(equalTo(expected)));
+    }
+
     @Test
     public void get_one_when_I_pass_one() {
-        assertThat("get 1 when I pass 1", FizzBuzz.fizzBuzz(1), is(equalTo("1")));
+        checkFizzBuzz(1, "1");
     }
 
     @Test
     public void get_two_when_I_pass_two() {
-        assertThat("get 2 when I pass 2", FizzBuzz.fizzBuzz(2), is(equalTo("2")));
+       checkFizzBuzz(2, "2");
     }
 }
