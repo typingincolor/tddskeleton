8207e8644989dbeb3b3b7e02200110fc1b50ab5a
Andrew Braithwaite <andrewbraithwaite@rideways.com>
2019-06-20 14:39:21 +0100

    test case 6: refactor

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,9 +1,13 @@
 public class FizzBuzz {
     public static String fizzBuzz(int i) {
-        if (i % 3 == 0) return "fizz";
+        if (isMultiple(i, 3)) return "fizz";
 
-        if (i % 5 == 0) return "buzz";
+        if (isMultiple(i,5)) return "buzz";
 
         return Integer.toString(i);
+    }
+
+    private static boolean isMultiple(int value, int mod) {
+        return value % mod == 0;
     }
 }
