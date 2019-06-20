997e03f05ed20e2f02cbaf8ca4dd2706dfd65bee
Andrew Braithwaite <andrewbraithwaite@rideways.com>
2019-06-20 13:48:27 +0100

    refactor: don't need the test to check we can call fizz buzz

--- src/test/java/TestFizzBuzz.java
+++ src/test/java/TestFizzBuzz.java
@@ -6,11 +6,6 @@ import static org.hamcrest.Matchers.is;
 
 public class TestFizzBuzz {
     @Test
-    public void it_can_call_fizzbuzz() {
-        FizzBuzz.fizzBuzz(1);
-    }
-
-    @Test
     public void get_one_when_I_pass_one() {
         assertThat("get 1 when I pass 1", FizzBuzz.fizzBuzz(1), is(equalTo("1")));
     }
