3b1a1871d32802b57d3321684aab741d715197f8
Andrew Braithwaite <andrewbraithwaite@rideways.com>
2019-06-20 13:46:35 +0100

    get "1" when I pass 1 test created and fails.

--- src/test/java/TestFizzBuzz.java
+++ src/test/java/TestFizzBuzz.java
@@ -1,8 +1,17 @@
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 public class TestFizzBuzz {
     @Test
     public void it_can_call_fizzbuzz() {
         FizzBuzz.fizzBuzz(1);
+    }
+
+    @Test
+    public void get_one_when_I_pass_one() {
+        assertThat("get 1 when I pass 1", FizzBuzz.fizzBuzz(1), is(equalTo("1")));
     }
 }
