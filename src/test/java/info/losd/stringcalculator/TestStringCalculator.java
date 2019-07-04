3a37ff3f418aa6c71c29d71fa3d2354fced23956
Andrew Braithwaite <andrewbraithwaite@rideways.com>
2019-07-04 13:03:28 +0100

    first failing test: test I can call it

---
+++ src/test/java/info/losd/stringcalculator/TestStringCalculator.java
@@ -0,0 +1,11 @@
+package info.losd.stringcalculator;
+
+import org.junit.Test;
+import static info.losd.stringcalculator.Calculator.add;
+
+public class TestStringCalculator {
+    @Test
+    public void test_i_can_call_it() {
+        add("");
+    }
+}
