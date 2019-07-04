5ba83d78e6be0d5e9f959934a039e155dfd35d84
Andrew Braithwaite <andrewbraithwaite@rideways.com>
2019-07-04 13:20:02 +0100

    third & fourth failing tests

--- src/test/java/info/losd/stringcalculator/TestStringCalculator.java
+++ src/test/java/info/losd/stringcalculator/TestStringCalculator.java
@@ -2,6 +2,8 @@ package info.losd.stringcalculator;
 
 import org.junit.Test;
 import static info.losd.stringcalculator.Calculator.add;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class TestStringCalculator {
     @Test
@@ -12,5 +14,15 @@ public class TestStringCalculator {
     @Test(expected = RuntimeException.class)
     public void when_more_than_2_numbers_then_exception_is_thrown() {
         add("1,2,3");
+    }
+
+    @Test
+    public void returns_zero_for_empty_string() {
+        assertThat(add(""), is(equalTo(0)));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void when_invalid_number_then_exception_is_thrown() {
+        add("1,x");
     }
 }
