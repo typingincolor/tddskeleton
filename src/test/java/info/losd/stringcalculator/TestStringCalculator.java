317d4d2a3a329fb5add30c770d9e62a9a02c510d
Andrew Braithwaite <andrewbraithwaite@rideways.com>
2019-07-04 13:10:02 +0100

    when_more_than_2_numbers_then_exception_is_thrown

--- src/test/java/info/losd/stringcalculator/TestStringCalculator.java
+++ src/test/java/info/losd/stringcalculator/TestStringCalculator.java
@@ -8,4 +8,9 @@ public class TestStringCalculator {
     public void test_i_can_call_it() {
         add("");
     }
+
+    @Test(expected = RuntimeException.class)
+    public void when_more_than_2_numbers_then_exception_is_thrown() {
+        add("1,2,3");    
+    }
 }
